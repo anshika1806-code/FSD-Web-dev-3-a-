1aeb55203d7afd3b0e41529df48708377874a55d
Anshika Gupta <anshikagupta1318@gmail.com>
2026-08-11 23:10:10 +0530

    Initial commit

---
+++ lecture 3/script.js
@@ -0,0 +1,8 @@
+const math = require("./math.js");
+
+// console.log(math);
+// //console.log("hello from lecture 3");
+
+
+console.log(math.add(2, 3));//output 5
+console.log(math.subtract(5, 3));//output 2
