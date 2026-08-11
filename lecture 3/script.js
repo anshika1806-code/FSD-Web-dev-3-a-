8b5cedd2fc99704b71aafb7ba0247ac1f12e11c3
Anshika Gupta <anshikagupta1318@gmail.com>
2026-08-11 23:12:45 +0530

    Updated files

--- lecture 3/script.js
+++ lecture 3/script.js
@@ -1,8 +1,14 @@
-const math = require("./math.js");
-
+//const math = require("./math.js");cs module
+import {add, subtract} from "./math.js"; //es module
+//const os=require("os");
+//const express=require("express");
 // console.log(math);
-// //console.log("hello from lecture 3");
+// console.log("hello from lecture 3");
 
 
-console.log(math.add(2, 3));//output 5
-console.log(math.subtract(5, 3));//output 2
+console.log(add(2, 3));//output 5
+console.log(subtract(5, 3));//output 2
+//console.log(os.platform());//output win32
+//console.log(os.arch());//output x64
+//console.log(os.cpus().length);//output number of CPU cores
+//console.log(os.freemem());//output free memory
